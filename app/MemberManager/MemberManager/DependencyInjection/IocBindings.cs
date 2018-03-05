f634053384b940676416cdec9d55fcab7324bbe3
Rob Horton <robhorton@outlook.com>
2018-03-04 20:29:53 -0600

    Adding first webapi endpoint and interface.  Adding Ninject.

---
+++ app/MemberManager/MemberManager/DependencyInjection/IocBindings.cs
@@ -0,0 +1,12 @@
+﻿using Ninject.Modules;
+
+namespace MemberManager.DependencyInjection
+{
+    public class IocBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
