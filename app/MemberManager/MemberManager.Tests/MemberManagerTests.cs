f2734491e9d6f2704fb10ea70a752b4aebeeb261
Rob Horton <robhorton@outlook.com>
2018-03-10 17:08:08 -0600

    Fixing up base unit test skeleton.

---
+++ app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace MemberManager.Tests
+{
+    [TestClass]
+    public class MemberManagerTests
+    {
+        [TestMethod]
+        public void TestGetMember()
+        {
+            var results = 1;
+            results.Should().NotBe(0);
+
+        }
+    }
+}
