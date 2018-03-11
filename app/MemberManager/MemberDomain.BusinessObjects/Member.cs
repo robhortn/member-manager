5e9cf7b8425907644c91a0e2f01b72e1de2ba808
Rob Horton <robhorton@outlook.com>
2018-03-10 21:04:34 -0600

    Adding new projects.

---
+++ app/MemberManager/MemberDomain.BusinessObjects/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberManager.BusinessObjects
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public bool Active { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public System.DateTime LastModifiedDate { get; set; }
+    }
+}
---
+++ app/MemberManager/MemberManager.Data/Helpers/Mappings.cs
@@ -0,0 +1,25 @@
+﻿using MemberManager.Data.EF;
+using System;
+
+namespace MemberManager.Data.Helpers
+{
+    public static class Mappings
+    {
+        public static BusinessObjects.Member MapMember(Member member)
+        {
+            if (member == null) throw new NullReferenceException("member is null in params list");
+
+            try
+            {
+                BusinessObjects.Member objMember = new BusinessObjects.Member();
+                objMember.Id = member.Id;
+
+                return objMember;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
