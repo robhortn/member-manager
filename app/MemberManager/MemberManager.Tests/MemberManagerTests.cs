afa0fcc066bf577be778bde189f71206b6816bd9
Rob Horton <robhorton@outlook.com>
2018-03-25 18:32:22 -0500

    Adding unit test

--- app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
+++ app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
@@ -1,23 +1,33 @@
-﻿using System;
+﻿using Moq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
+
+using MemberManager.Data.Interface;
+using MemberManager.Domain.Interfaces;
+using MemberManager.Domain;
 
 namespace MemberManager.Tests
 {
     [TestClass]
     public class MemberManagerTests
     {
-        [TestInitialize]
-        public void Initialize() {
-            //IMemberDataWriter dw, IMemberRepo repo
-        }
+        MemberDomain _memberDomain = null;
+        Mock<IMemberDataWriter> _dw = null;
+        Mock<IMemberRepo> _repo = null;
+
+        //[TestInitialize]
+        //public void Initialize() {
+        //    _dw = new Mock<IMemberDataWriter>();
+        //    _repo = new Mock<IMemberRepo>();
+
+        //    _memberDomain = new MemberDomain(_dw.Object, _repo.Object);
+        //}
 
         [TestMethod]
         public void TestGetMember()
         {
-            var results = 1;
-            results.Should().NotBe(0);
-
+            var mock = new Mock<IMemberDomain>();
+            mock.Setup(x => x.GetMember(1)).Should().NotBeNull();
         }
     }
 }
