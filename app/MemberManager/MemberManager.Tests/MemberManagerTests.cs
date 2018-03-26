a0784c28a1f86ea1924efe74f5194ed5ea196b9a
Rob Horton <robhorton@outlook.com>
2018-03-25 20:56:27 -0500

    Adding basic unit tests

--- app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
+++ app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
@@ -2,7 +2,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 
-using MemberManager.Domain;
 using MemberManager.Interfaces;
 using MemberManager.BusinessObjects;
 
@@ -11,30 +10,18 @@ namespace MemberManager.Tests
     [TestClass]
     public class MemberManagerTests
     {
-        MemberDomain _memberDomain = null;
-        Mock<IMemberDataWriter> _dw = null;
-        Mock<IMemberRepo> _repo = null;
-
-        //[TestInitialize]
-        //public void Initialize() {
-        //    _dw = new Mock<IMemberDataWriter>();
-        //    _repo = new Mock<IMemberRepo>();
-
-        //    _memberDomain = new MemberDomain(_dw.Object, _repo.Object);
-        //}
-
         [TestMethod]
         public void Get()
         {
             var mock = new Mock<IMemberDomain>();
-            //mock.Setup(x => x.Get().Should().NotBeNullOrEmpty();
+            mock.Setup(x => x.Get()).Should().NotBeNull();
         }
 
         [TestMethod]
         public void GetMember()
         {
             var mock = new Mock<IMemberDomain>();
-            mock.Setup(x => x.GetMember(1)).Should().NotBeNull();
+            mock.Setup(x => x.GetMember(It.IsAny<int>())).Should().NotBeNull();
         }
 
         [TestMethod]
@@ -48,8 +35,7 @@ namespace MemberManager.Tests
         public void Save()
         {
             var mock = new Mock<IMemberDomain>();
-            var newMember = new Member { Active = true, FirstName = "Bat", LastName = "Man" };
-            //mock.Setup(x => x.Save(newMember).Should().NotBe(null);
+            mock.Setup(x => x.Save(It.IsAny<Member>())).Returns(1);
         }
     }
 }
