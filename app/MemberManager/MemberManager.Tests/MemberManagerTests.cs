472093c10d0ac4eae17eb073a44f9df18fcadcdf
Rob Horton <robhorton@outlook.com>
2018-03-25 19:45:31 -0500

    Adding unit test mocks

--- app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
+++ app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 
 using MemberManager.Domain;
 using MemberManager.Interfaces;
+using MemberManager.BusinessObjects;
 
 namespace MemberManager.Tests
 {
@@ -23,10 +24,32 @@ namespace MemberManager.Tests
         //}
 
         [TestMethod]
-        public void TestGetMember()
+        public void Get()
+        {
+            var mock = new Mock<IMemberDomain>();
+            //mock.Setup(x => x.Get().Should().NotBeNullOrEmpty();
+        }
+
+        [TestMethod]
+        public void GetMember()
         {
             var mock = new Mock<IMemberDomain>();
             mock.Setup(x => x.GetMember(1)).Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            var mock = new Mock<IMemberDomain>();
+            mock.Setup(x => x.Delete(It.Is<int>(i => true))).Returns(true);
+        }
+
+        [TestMethod]
+        public void Save()
+        {
+            var mock = new Mock<IMemberDomain>();
+            var newMember = new Member { Active = true, FirstName = "Bat", LastName = "Man" };
+            //mock.Setup(x => x.Save(newMember).Should().NotBe(null);
         }
     }
 }
