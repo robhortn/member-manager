d9eb0dce7e77bdb0bc3e1d7c263cb5fa150fdc55
Rob Horton <robhorton@outlook.com>
2018-04-02 13:16:21 -0500

    Updating unit tests.

--- app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
+++ app/MemberManager/MemberManager.Tests/MemberManagerTests.cs
@@ -22,8 +22,10 @@ namespace MemberManager.Tests
         [TestMethod]
         public void GetMember()
         {
-            var mock = new Mock<IMemberDomain>();
-            mock.Setup(x => x.GetMember(It.IsAny<int>())).Should().NotBeNull();
+            Data.MemberRepo repo = new Data.MemberRepo();
+            repo.Save(new Member { FirstName = "Ben", LastName = "Kenobi" });
+            repo.Save(new Member { FirstName = "Han", LastName = "Solo" });
+            repo.Get().Count().Should().Be(2);
         }
 
         [TestMethod]
@@ -41,8 +43,9 @@ namespace MemberManager.Tests
         [TestMethod]
         public void Save()
         {
-            var mock = new Mock<IMemberDomain>();
-            mock.Setup(x => x.Save(It.IsAny<Member>())).Returns(It.IsAny<int>());
+            Data.MemberRepo repo = new Data.MemberRepo();
+            repo.Save(new Member { FirstName = "Ben", LastName = "Kenobi" });
+            repo.Get().Count().Should().Be(1);
         }
     }
 }
